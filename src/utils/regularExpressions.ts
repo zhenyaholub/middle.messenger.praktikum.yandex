38941548b354e476c988392d104d872fec77b592
Zhenya Holub <zhenya.holub@rdigital.by>
2023-04-18 14:44:49 +0300

    regular expressions nave been added

--- src/utils/regularExpressions.ts
+++ src/utils/regularExpressions.ts
@@ -1,5 +1,20 @@
-import { LOGIN } from "./fieldNames";
+import {
+  EMAIL,
+  FIRST_NAME,
+  LOGIN,
+  MESSAGE,
+  PASSWORD,
+  PHONE,
+  SECOND_NAME,
+} from "./fieldNames";
 
 export const REGULAR_EXPRESSIONS = {
   [LOGIN]: /(?!^\d+$)^[a-zA-Z0-9_-]{3,20}$/,
+  [PASSWORD]: /^(?=.*\d)(?=.*[A-Z])[a-zA-Z0-9]{8,40}$/,
+  [MESSAGE]: /.+/,
+  [PHONE]: /^(\+)?\d{10,15}$/g,
+  [EMAIL]:
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(\.)[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+  [FIRST_NAME]: /^[A-ZА-Я]+[A-ZА-Я-]*$/i,
+  [SECOND_NAME]: /^[A-ZА-Я]+[A-ZА-Я-]*$/i,
 };
