39223b74e8b7fe469ec3af8b678b455f6f6ab004
Zhenya Holub <zhenya.holub@rdigital.by>
2023-05-07 15:09:18 +0300

    router handlers have been added to components

--- src/pages/signIn/content.ts
+++ src/pages/signIn/content.ts
@@ -1,70 +1,73 @@
-import { Button } from '../../components/button/button'
-import { Field } from '../../components/field/field'
-import { Form } from '../../components/form/form'
-import { Icons8 } from '../../components/icons8/icons8'
-import { Label } from '../../components/label/label'
-import { Link } from '../../components/link/link'
-import { Title } from '../../components/title/title'
-import { Input } from '../../components/input/input'
+import { Button } from "../../components/button/button";
+import { Field } from "../../components/field/field";
+import { Form } from "../../components/form/form";
+import { Icons8 } from "../../components/icons8/icons8";
+import { Label } from "../../components/label/label";
+import { Link } from "../../components/link/link";
+import { Title } from "../../components/title/title";
+import { Input } from "../../components/input/input";
+import { ErrorMessage } from "../../components/errorMessage/errorMessage";
 
-import { LOGIN, PASSWORD } from '../../utils/fieldNames'
-import { validationSignIn } from '../../utils/mediator'
-import { ErrorMessage } from '../../components/errorMessage/errorMessage'
+import { LOGIN, PASSWORD } from "../../utils/fieldNames";
+import { validationSignIn } from "../../utils/mediator";
+import { SIGN_UP } from "../../utils/urls";
+import { router } from "../../utils/router";
 
-const title = new Title({ text: 'Вход' })
+const title = new Title({ text: "Вход" });
 
-const loginLabel = new Label({ for: LOGIN, text: 'Логин' })
+const loginLabel = new Label({ for: LOGIN, text: "Логин" });
 const loginInput = new Input({
   name: LOGIN,
   id: LOGIN,
-  type: 'text',
-  placeholder: 'Vitalik',
-  value: '',
-  class: 'input',
-  mediator: validationSignIn
-})
-const errorMessageLogin = new ErrorMessage({ message: 'Неверный логин' })
+  type: "text",
+  placeholder: "Vitalik",
+  value: "",
+  class: "input",
+  mediator: validationSignIn,
+});
+const errorMessageLogin = new ErrorMessage({ message: "Неверный логин" });
 export const login = new Field({
   label: loginLabel,
   input: loginInput,
   name: LOGIN,
-  errorMessage: errorMessageLogin
-})
+  errorMessage: errorMessageLogin,
+});
 
-const passwordLabel = new Label({ for: PASSWORD, text: 'Пароль' })
+const passwordLabel = new Label({ for: PASSWORD, text: "Пароль" });
 const passwordInput = new Input({
   name: PASSWORD,
   id: PASSWORD,
-  type: 'password',
-  placeholder: '...........',
-  class: 'input',
-  mediator: validationSignIn
-})
-const errorMessagePassword = new ErrorMessage({ message: 'Неверный пароль' })
+  type: "password",
+  placeholder: "...........",
+  class: "input",
+  mediator: validationSignIn,
+});
+const errorMessagePassword = new ErrorMessage({ message: "Неверный пароль" });
 export const password = new Field({
   label: passwordLabel,
   input: passwordInput,
-  style: 'margin:0 0 159px 0',
+  style: "margin:0 0 159px 0",
   name: PASSWORD,
-  errorMessage: errorMessagePassword
-})
+  errorMessage: errorMessagePassword,
+});
 
-const fields = [login, password]
-const topButton = new Button({ type: 'submit', text: 'Авторизоваться' })
+const fields = [login, password];
+const topButton = new Button({ type: "submit", text: "Авторизоваться" });
 const bottomButton = new Link({
-  text: 'Нет аккаунта?'
-})
+  text: "Нет аккаунта?",
+  events: { click: () => router.go(SIGN_UP) },
+});
 
 const form = new Form({
   title,
   fields,
   topButton,
   bottomButton,
-  mediator: validationSignIn
-})
+  mediator: validationSignIn,
+});
 
-const icons8 = new Icons8({})
+const icons8 = new Icons8({});
 
-export const signInProps = { form, icons8 }
+export const signInProps = { form, icons8 };
 
-validationSignIn.add(fields)
+validationSignIn.add(fields);
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,68 +1,70 @@
-import { type Block } from './block'
-import { REGULAR_EXPRESSIONS } from './regularExpressions'
+import { RoutesType } from "../types/router";
+import { type Block } from "./block";
+import { REGULAR_EXPRESSIONS } from "./regularExpressions";
+import { router } from "./router";
 import {
   SERVER_ERROR,
   EDIT_PROFILE,
   EDIT_PASSWORD,
-  PROFILE,
+  SETTINGS,
   NOT_FOUND,
   SIGN_UP,
-  SIGN_IN
-} from './urls'
+  SIGN_IN,
+} from "./urls";
 
-export function render (query: string, block: Block) {
-  const element = document.querySelector(query)
-  const blockTemplate = block.getContent()
-  element?.append(blockTemplate!)
+export function render(query: string, block: Block) {
+  const element = document.querySelector(query);
+  const blockTemplate = block.getContent();
+  element?.append(blockTemplate!);
 }
 
-export function setBodyStyles () {
-  const body = document.body
-  body.className = ''
+export function setBodyStyles() {
+  const body = document.body;
+  body.className = "";
 
   switch (location.pathname) {
     case SIGN_IN:
-      body.classList.add('flexCenter', 'padding')
-      break
+      body.classList.add("flexCenter", "padding");
+      break;
     case SIGN_UP:
-      body.classList.add('flexCenter', 'padding')
-      break
+      body.classList.add("flexCenter", "padding");
+      break;
     case NOT_FOUND:
-      body.classList.add('flexCenter', 'padding')
-      break
+      body.classList.add("flexCenter", "padding");
+      break;
     case SERVER_ERROR:
-      body.classList.add('flexCenter', 'padding')
-      break
-    case PROFILE:
-      body.classList.add('profileBody', 'padding2')
-      break
+      body.classList.add("flexCenter", "padding");
+      break;
+    case SETTINGS:
+      body.classList.add("profileBody", "padding2");
+      break;
     case EDIT_PROFILE:
-      body.classList.add('profileBody', 'padding2')
-      break
+      body.classList.add("profileBody", "padding2");
+      break;
     case EDIT_PASSWORD:
-      body.classList.add('profileBody', 'padding2')
-      break
+      body.classList.add("profileBody", "padding2");
+      break;
     default:
-      break
+      break;
   }
 }
 
-export function queryStringify (data: object) {
-  let query = '?'
-  const entries = Object.entries(data)
+export function queryStringify(data: object) {
+  let query = "?";
+  const entries = Object.entries(data);
   entries.forEach(([key, value], index, array) => {
-    const isLast = index === array.length - 1
-    query += `${key}=${value}${!isLast ? '&' : ''}`
-  })
-  return query
+    const isLast = index === array.length - 1;
+    query += `${key}=${value}${!isLast ? "&" : ""}`;
+  });
+  return query;
 }
 
-export function validateInput (value: string, name: string) {
+export function validateInput(value: string, name: string) {
   return REGULAR_EXPRESSIONS[name as keyof typeof REGULAR_EXPRESSIONS].test(
     value
-  )
+  );
 }
 
-export function isEqual (lhs: string, rhs: string) {
-  return lhs === rhs
+export function isEqual(lhs: string, rhs: string) {
+  return lhs === rhs;
 }
